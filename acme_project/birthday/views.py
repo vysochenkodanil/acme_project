57857b6990149c1d449704df76c8cf84d188144c
Данил Высоченко <vysochenkodanil@gmail.com>
2025-01-04 15:52:13 +0300

    редактировал шаблоны

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,6 +1,22 @@
+# birthday/views.py 
 from django.shortcuts import render
+
+from .forms import BirthdayForm
+# Импортируем из utils.py функцию для подсчёта дней.
+from .utils import calculate_birthday_countdown
 
 
 def birthday(request):
-    context = {}
-    return render(request, 'birthday/birthday.html', context=context)
+    form = BirthdayForm(request.GET or None)
+    # Создаём словарь контекста сразу после инициализации формы.
+    context = {'form': form}
+    # Если форма валидна...
+    if form.is_valid():
+        # ...вызовем функцию подсчёта дней:
+        birthday_countdown = calculate_birthday_countdown(
+            # ...и передаём в неё дату из словаря cleaned_data.
+            form.cleaned_data['birthday']
+        )
+        # Обновляем словарь контекста: добавляем в него новый элемент.
+        context.update({'birthday_countdown': birthday_countdown})
+    return render(request, 'birthday/birthday.html', context)
