38f899ffde2697ab1967e8ee21049d576f60572f
Данил Высоченко <vysochenkodanil@gmail.com>
2025-01-06 19:44:18 +0300

    замена view class

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 from .validators import real_age
 
 class Birthday(models.Model):
@@ -17,3 +17,6 @@ class Birthday(models.Model):
                 name='Unique person constraint',
             ),
         )
+    def get_absolute_url(self):
+        # С помощью функции reverse() возвращаем URL объекта.
+        return reverse('birthday:detail', kwargs={'pk': self.pk})
