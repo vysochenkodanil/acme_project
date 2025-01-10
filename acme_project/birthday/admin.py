430570b16de1b5d37b41a4b62c847c83625255cc
Данил Высоченко <vysochenkodanil@gmail.com>
2025-01-10 15:32:35 +0300

    доработка шаблона

--- acme_project/birthday/admin.py
+++ acme_project/birthday/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Birthday
+from .models import Birthday, Tag
 
 # ...и регистрируем её в админке:
 admin.site.register(Birthday) 
+admin.site.register(Tag)
--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -6,6 +6,14 @@ from django.contrib.auth import get_user_model
 # Да, именно так всегда и ссылаемся на модель пользователя!
 User = get_user_model()
 
+
+class Tag(models.Model):
+    tag = models.CharField('Тег', max_length=20)
+
+    def __str__(self):
+        return self.tag
+
+
 class Birthday(models.Model):
     first_name = models.CharField('Имя', max_length=20)
     last_name = models.CharField(
@@ -15,6 +23,12 @@ class Birthday(models.Model):
     image = models.ImageField('Фото', upload_to='birthdays_images', blank=True)
     author = models.ForeignKey(
         User, verbose_name='Автор записи', on_delete=models.CASCADE, null=True
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Теги',
+        blank=True,
+        help_text='Удерживайте Ctrl для выбора нескольких вариантов'
     )
 
     class Meta:
@@ -43,3 +57,4 @@ class Congratulation(models.Model):
 
     class Meta:
         ordering = ('created_at',)
+
